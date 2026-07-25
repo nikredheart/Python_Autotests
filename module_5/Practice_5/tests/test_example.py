457befa48eb8d90a22b1ab50c7d6b84bdac7a527
Nick <nikredheart.official@yandex.ru>
2026-07-25 15:39:49 +0300

    Практическая работа №6

--- module_5/Practice_5/tests/test_example.py
+++ module_5/Practice_5/tests/test_example.py
@@ -9,10 +9,10 @@ class TestExample:
     def test_1(self, browser):
         browser.get('https://github.com/microsoft/vscode/issues')
 
-        (browser.find_element(By.CSS_SELECTOR, '[id="repository-input"]').send_keys('in:title bug' + Keys.ENTER))
+        browser.find_element(By.CSS_SELECTOR, '[id="repository-input"]').send_keys('in:title bug' + Keys.ENTER)
         time.sleep(5)
 
-        indexes = [4, 11, 13, 17, 18, 19, 21, 22, 23, 24, 25, 26, 33, 34, 35, 36, 38, 42, 45, 46, 47, 50, 51, 52, 55]
+        indexes = [3, 4, 7, 11, 12, 14, 21, 23, 27, 28, 29, 30, 31, 32, 33, 34, 35, 42, 43, 44, 45, 49, 52, 53, 54]
 
         for index in indexes:
             checking_task_name = (browser.find_element(By.XPATH, f'(//*[@class="prc-Text-Text-9mHv3"])[{index}]'))
