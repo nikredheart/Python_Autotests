942386f5e9ce6fb1cb31baba806491ccc5869c20
nikredheart <nikredheart.official@gmail.com>
2026-08-29 15:21:53 +0300

    Delete module_6/Practice_6 directory

--- module_6/Practice_6/conftest.py
+++
@@ -1,9 +0,0 @@
-import logging.config
-from os import path
-
-pytest_plugins = [
-        'src.browser'
-]
-
-log_file_path = path.join(path.dirname(path.abspath(__file__)), 'logging.ini')
-logging.config.fileConfig(log_file_path)
